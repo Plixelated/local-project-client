32217f52b24c7fc64887e31bf9aaeb4aba9e96c1
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-05-01 00:07:34 -0700

    fix: updated api location

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { LoginRequest } from './login-request';
 import { LoginResponse } from './login-response';
 import { BehaviorSubject, Observable, pipe, tap } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
